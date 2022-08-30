ce7354836ada2420a428568ffd265e0ed43ccb23
Paulo Martins <paulo@occupop.com>
2022-08-30 20:47:37 -0300

    added index configs

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route, Navigate, useLocation, Outlet} from 'react-router-dom';
+import './styles/index.css';
+import './styles/index-sass';
+import FindUser from './domain/finder/find/FindUser';
+import FindResult from './domain/finder/result/FindResult';
+import reportWebVitals from './config/reportWebVitals';
+
+const StateParamWrapper = () => {
+  const {state} = useLocation();
+  return state ? <Outlet /> : <Navigate to="/" replace />;
+}
+
+const RoutingPages = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/find" />} />
+        <Route path="/find" element={<FindUser />} />
+        <Route path="*" element={<FindUser />} />
+        <Route element={<StateParamWrapper />}>
+          <Route path="/result" element={<FindResult />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RoutingPages />
+  </React.StrictMode>
+)
+
+reportWebVitals();
---
+++ src/styles/index-sass.js
@@ -0,0 +1,5 @@
+import '../components/card/card.scss';
+import '../components/header/header.scss';
+import '../components/footer/footer.scss';
+import '../domain/finder/find/find-user.scss';
+import '../domain/finder/result/find-result.scss';
