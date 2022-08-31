2806b6bd1055c7c4cd2a50e77dadd9105caf9022
Paulo Martins <paulo@occupop.com>
2022-08-30 21:33:13 -0300

    fix tests

--- src/domain/finder/find/FindUser.test.js
+++ src/domain/finder/find/FindUser.test.js
@@ -1,8 +1,21 @@
 import {render} from '@testing-library/react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import FindUser from './FindUser';
 
 test('render header tag', () => {
-  const {container} = render(<FindUser />);
+  const {container} = render(<BrowserRouter><Routes><Route path="/" element={<FindUser />} /></Routes></BrowserRouter>);
   const header = container.querySelector('header');
   expect(header).toBeDefined();
+});
+
+test('render footer tag', () => {
+  const {container} = render(<BrowserRouter><Routes><Route path="/" element={<FindUser />} /></Routes></BrowserRouter>);
+  const footer = container.querySelector('footer');
+  expect(footer).toBeDefined();
+});
+
+test('render component body element', () => {
+  const {container} = render(<BrowserRouter><Routes><Route path="/" element={<FindUser />} /></Routes></BrowserRouter>);
+  const body = container.querySelector('find-user__body');
+  expect(body).toBeDefined();
 });
